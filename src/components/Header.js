2093589febaf36daccbdec02a9e22db6e89e6262
felix1805 <felix1805@gmail.com>
2023-07-14 17:04:53 -0400

    structuring Header.js, added styling and layout to app.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,29 @@
 const Header = () => {
   return (
-  <header></header>
+    <header>
+      <div className="info-container">
+        <div className="user-info-container">
+          <h1>username</h1>
+          <p>handle <span className="threads-info">threads.net</span></p>
+          <div className="img-container">
+            <img src="" alt="profile avatar" />
+          </div>
+        </div>
+        <p>bio</p>
+        <div className="sub-info-container">
+          <p className="sub-text">X followers • <a href="">link</a></p>
+        </div>
+        <button
+          className="primary"
+          onClick={() => navigator.clipboard.writeText('I am a URL')}>
+          Share Profile
+        </button>
+        <div className="button-container">
+          <button className="current">Threads</button>
+          <button>Replies</button>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,11 +1,11 @@
 import PopUpThread from "./PopUpThread";
-import ThreadInput from "./PopUpThread";
+import ThreadInput from "./ThreadInput";
 
 const PopUp = () => {
   return (
     <div className="popup">
-      <PopUpThread></PopUpThread>
-      <ThreadInput></ThreadInput>
+      <PopUpThread/>
+      <ThreadInput/>
     </div>
   )
 }
