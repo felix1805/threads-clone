7aa98e742efc82cd8cbda0b591ca6423c688d362
felix1805 <felix1805@gmail.com>
2023-07-14 16:34:25 -0400

    created components folder, defined needed components, added imports and began styling layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import PopUp from "./components/PopUp";
+
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
+      <Nav></Nav>
+      <Header></Header>
+      <Feed></Feed>
+      <PopUp></PopUp>
     </div>
   );
 }
---
+++ src/components/PopUp.js
@@ -0,0 +1,13 @@
+import PopUpThread from "./PopUpThread";
+import ThreadInput from "./PopUpThread";
+
+const PopUp = () => {
+  return (
+    <div className="popup">
+      <PopUpThread></PopUpThread>
+      <ThreadInput></ThreadInput>
+    </div>
+  )
+}
+
+export default PopUp;
